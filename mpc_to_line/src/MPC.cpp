f1c5d38f54811e8537e7456ecb81c8ef492e1278
Jan Grodowski <jgrodowski@gmail.com>
2017-08-10 21:03:57 +0200

    implement solver equations for mpc_to_line

--- mpc_to_line/src/MPC.cpp
+++ mpc_to_line/src/MPC.cpp
@@ -11,8 +11,8 @@ namespace plt = matplotlibcpp;
 using CppAD::AD;
 
 // TODO: Set N and dt
-size_t N = ? ;
-double dt = ? ;
+size_t N = 25;
+double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -56,16 +56,35 @@ class FG_eval {
     // Any additions to the cost should be added to `fg[0]`.
     fg[0] = 0;
 
-    // Reference State Cost
+    // Cost function
     // TODO: Define the cost related the reference state and
     // any anything you think may be beneficial.
+
+    // The part of the cost based on the reference state.
+    for (int t = 0; t < N; t++) {
+      fg[0] += CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
+    }
+
+    // Minimize the use of actuators.
+    for (int t = 0; t < N - 1; t++) {
+      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += CppAD::pow(vars[a_start + t], 2);
+    }
+
+    // Minimize the value gap between sequential actuations.
+    for (int t = 0; t < N - 2; t++) {
+      fg[0] += CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+    }
 
     //
     // Setup Constraints
     //
     // NOTE: In this section you'll setup the model constraints.
 
-    // Initial constraints
+    // Initial constraints (initial vehicle state)
     //
     // We add 1 to each of the starting indices due to cost being located at
     // index 0 of `fg`.
@@ -78,12 +97,30 @@ class FG_eval {
     fg[1 + epsi_start] = vars[epsi_start];
 
     // The rest of the constraints
-    for (int t = 1; t < N; t++) {
+    for (int t = 1; t < N - 1; t++) {
+      // variable 0 is state at time t
+      // variable 1 is state at time t + 1
       AD<double> x1 = vars[x_start + t];
-
       AD<double> x0 = vars[x_start + t - 1];
+      
+      AD<double> y1 = vars[y_start + t];
+      AD<double> y0 = vars[y_start + t - 1];
+
+      AD<double> psi1 = vars[psi_start + t];
       AD<double> psi0 = vars[psi_start + t - 1];
+      
+      AD<double> v1 = vars[v_start + t];
       AD<double> v0 = vars[v_start + t - 1];
+
+      AD<double> cte1 = vars[cte_start + t];
+      AD<double> cte0 = vars[cte_start + t - 1];
+
+      AD<double> epsi1 = vars[epsi_start + t];
+      AD<double> epsi0 = vars[epsi_start + t - 1];
+
+      // current state steering rate and acceleration
+      AD<double> delta0 = vars[delta_start + t] ;
+      AD<double> a0     = vars[a_start + t] ;
 
       // Here's `x` to get you started.
       // The idea here is to constraint this value to be 0.
@@ -92,8 +129,23 @@ class FG_eval {
       // This is also CppAD can compute derivatives and pass
       // these to the solver.
 
-      // TODO: Setup the rest of the model constraints
+      // implement state change equations from lecture
+      // https://youtu.be/CZ71uEy8EtI?t=27
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[1 + psi_start + t] = psi1 - (psi0 + (v0 / Lf) * delta0 * dt);
+      fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+
+      // TODO: uncomment for 3rd order coeffs
+      // AD<double> cte_f_x0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0;
+      AD<double> cte_f_x0 = coeffs[0]; //+ coeffs[1] * x0;
+
+      fg[1 + cte_start + t] = cte1 - (cte_f_x0 - y0 + v0 * CppAD::sin(epsi0) * dt);
+
+      // TODO: uncomment for 3rd order coeffs
+      // AD<double> cte_f_x0_prim = coeffs[1] + coeffs[2] * x0;
+      AD<double> cte_f_x0_prim = 0;
+      fg[1 + epsi_start + t] = epsi1 - (psi0 - CppAD::atan(cte_f_x0_prim) + (v0 / Lf) * delta0 * dt);
     }
   }
 };
@@ -263,7 +315,8 @@ int main() {
   ptsy << -1, -1;
 
   // TODO: fit a polynomial to the above x and y coordinates
-  auto coeffs = ? ;
+  // TODO: use 3rd order polynomial in real MPC project
+  auto coeffs = polyfit(ptsx, ptsy, 1);
 
   // NOTE: free feel to play around with these
   double x = -1;
@@ -271,9 +324,9 @@ int main() {
   double psi = 0;
   double v = 10;
   // TODO: calculate the cross track error
-  double cte = ? ;
+  double cte = polyeval(coeffs, x) - y;
   // TODO: calculate the orientation error
-  double epsi = ? ;
+  double epsi = psi - atan(coeffs[1]);
 
   Eigen::VectorXd state(6);
   state << x, y, psi, v, cte, epsi;
